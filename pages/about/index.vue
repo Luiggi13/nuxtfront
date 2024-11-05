1695955847d4ee905307cadd02a529bec3f11583
Christian Llansola <christian.llansola@mail.schwarz>
2024-11-05 17:55:19 +0100

    change categories

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -18,7 +18,7 @@
             </div>
             <div class="w-full">
                 <div class="w-full h-8 px-3 items-center flex">
-                    <h6 class="text-gray-500 text-xs font-semibold leading-4">MENU</h6>
+                    <h6 class="text-gray-500 text-xs font-semibold leading-4">Categories</h6>
                 </div>
                 <ul class="flex-col gap-1 flex">
                     <li v-for="(menu, idx) in menus" :key="idx">
@@ -69,7 +69,7 @@ type itemsMenu = {
             {
                 link: "/upload",
                 icon: '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none"><g id="Upload 02"><path id="icon" d="M8.1475 12.6968L9.99935 10.8333L11.8512 12.6968M9.99935 17.5V10.8795M5.36972 13.6805H4.92701C3.12601 13.6805 1.66602 12.2205 1.66602 10.4196C1.66602 8.61855 3.12601 7.15855 4.92701 7.15855V7.15855C5.17151 7.15855 5.36753 6.95973 5.39031 6.71629C5.61168 4.35087 7.59071 2.5 9.99935 2.5C12.5562 2.5 14.629 4.5857 14.629 7.15855H15.0717C16.8727 7.15855 18.3327 8.61855 18.3327 10.4196C18.3327 12.2205 16.8727 13.6805 15.0717 13.6805H14.629" stroke="#6B7280" strokeWidth="2.6" strokeLinecap="round" /></g></svg>',
-                text: "Upload ↑"
+                text: "Upload"
             },
             {
                 link: "/compressed",
