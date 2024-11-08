1c0a7a0186da2f059302727aa5606c3caff969ff
Christian Llansola <christian.llansola@mail.schwarz>
2024-11-08 17:21:46 +0100

    REmove sidebar

--- app.vue
+++ app.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-import Sidebar from '@/components/Sidebar.vue';
 </script>
-
 <template>
   <NuxtLoadingIndicator/>
   <div class="min-h-dvh h-dvh">
     <div class="w-full flex gap-4 min-h-dvh h-dvh">
-      <!-- <Sidebar /> -->
       <NuxtPage />
     </div>
   </div>
