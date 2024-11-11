c01b2e70e356e85672ea80f7e431b3c634d74cb8
Christian Llansola <christian.llansola@mail.schwarz>
2024-11-12 00:35:18 +0100

    Link name

--- Navigation.vue
+++ Navigation.vue
@@ -14,7 +14,7 @@
 						<NuxtLink to="/compressed"
 							exact-active-class="font-medium text-white bg-blue-700 md:text-white-700 px-3 dark:text-white md:dark:text-white block"
 							class="font-light text-xl px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-							Mis descargas</NuxtLink>
+							Mis documentos</NuxtLink>
 					</li>
 				</ul>
 			</div>
